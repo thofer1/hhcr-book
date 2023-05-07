e08ada78d86ec6d1c0c54625b2c4bd932aa9ab2c
thofer1 <thofer@umich.edu>
2023-05-07 12:34:33 -0400

    update from pull

--- render_git_push.sh
+++ render_git_push.sh
@@ -1,6 +1,6 @@
 cd /Users/thofer/Data/Rproj/hhcr-book
 git pull
 /Applications/RStudio.app/Contents/Resources/app/quarto/bin/quarto render
-git add -A && git commit -a -m "a message describing what you did" && git push origin main
+git add -A && git commit -a -m "update from pull" && git push origin main
 
 
