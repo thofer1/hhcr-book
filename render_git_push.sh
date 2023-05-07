c05f8214d379edda91acaf464e6686e96a3db807
thofer1 <thofer@umich.edu>
2023-05-07 12:16:34 -0400

    a message describing what you did

---
+++ render_git_push.sh
@@ -0,0 +1,5 @@
+cd /Users/thofer/Data/Rproj/hhcr-book
+/Applications/RStudio.app/Contents/Resources/app/quarto/bin/quarto render
+git add -A && git commit -a -m "a message describing what you did" && git push origin master
+
+
